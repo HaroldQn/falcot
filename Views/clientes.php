024cf7370f05580bdc89d22474d5fde2f6c8a882
LuisLlancari <miguel.llancari12@gmail.com>
2024-07-15 13:34:34 -0500

    cambios en clientes

--- Views/clientes.php
+++ Views/clientes.php
@@ -1,3 +1,41 @@
 <?php require_once './navbar.php'; ?>
+<h1 class="text-center">Clientes </h1>
+
+<div class="container container-sm">
+  <button type="button" class="btn btn-success mb-3">Agregar Cliente</button>
+  <table class="table table-bordered text-center" id="tabla-cliente">
+    <thead >
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Razon social</th>
+        <th scope="col">Ruc</th>
+        <th scope="col">Dirección</th>
+        <th scope="col">Telefono</th>
+        <th scope="col">Opciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark sssassaasdadds</td>
+        <td>Otto22222222</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+        <td>
+          <button type="button" data-id="${element.idusuario}" class="btn btn-sm btn-warning editar">Editar</button>
+          <button type="button" data-id="${element.idusuario}" class="eliminar btn btn-sm btn-danger ">Eliminar</button>
+        </td>
+
+      </tr>
+    </tbody>
+  </table>
+</div>
+<script>
+  document.addEventListener("DOMContentLoaded", () => {
+
+    
+
+  })
+</script>
 </body>
 </html>
